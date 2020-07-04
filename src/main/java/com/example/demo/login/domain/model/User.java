c2ba5dc7e15b2984e6ee2b009d587ab6b31d4ea3
Ryo Takanami <rtakanami0706gmail.com>
2020-07-04 22:48:43 +0900

    Lombok STSに適用

--- src/main/java/com/example/demo/login/domain/model/User.java
+++ src/main/java/com/example/demo/login/domain/model/User.java
@@ -6,55 +6,13 @@ import lombok.Data;
 
 @Data
 public class User {
-
-    private String userId; //ユーザーID
-    private String password; //パスワード
-    private String userName; //ユーザー名
-    private Date birthday; //誕生日
-    private int age; //年齢
-    private boolean marriage; //結婚ステータス
-    private String role; //ロール
-    
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public Date getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public boolean isMarriage() {
-		return marriage;
-	}
-	public void setMarriage(boolean marriage) {
-		this.marriage = marriage;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
+	
+	private String userId; // ユーザーID
+	private String password; // パスワード
+	private String userName; // ユーザー名
+	private Date birthday; // 誕生日
+	private int age; // 年齢
+	private boolean marriage; // 結婚ステータス
+	private String role; // ロール
+	
 }
--- src/main/java/com/example/demo/trySpring/Employee.java
+++ src/main/java/com/example/demo/trySpring/Employee.java
@@ -9,22 +9,4 @@ public class Employee {
     private String employeeName; //従業員名
     private int age; //年齢
     
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getEmployeeName() {
-		return employeeName;
-	}
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 }
